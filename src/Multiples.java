6ba51117cdc5e51de86106854898e75afc1e833e
chika <chika.peynisa@mail.utoronto.ca>
2024-09-14 21:54:52 -0400

    Adding Multiples class

---
+++ src/Multiples.java
@@ -0,0 +1,17 @@
+public class Multiples {
+
+
+    public static void main(String[] args) {
+
+        int num_divisible = 0;
+        for (int i = 0; i <= 1000; i++) {
+            if ((i % 3 == 0 || i % 5 == 0)) {
+                num_divisible++;
+            }
+        }
+
+        System.out.println(num_divisible);
+
+
+    }
+}
